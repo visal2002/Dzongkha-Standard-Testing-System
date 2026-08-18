0c3ca28e47373075a28656c41be31346c9ed45be
Vishal Chettri <chrttrivishal22@gmail.com>
2026-08-18 17:29:17 +0100

    feat: initialize React root and App component in main.jsx

--- dzongjuk-frontend/src/main.jsx
+++ dzongjuk-frontend/src/main.jsx
@@ -4,20 +4,14 @@
  * Phone: +975 - 1750 - 5267
  */
 
-console.log('MAIN.JSX IS EXECUTING!');
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import './i18n';
 
-import { ErrorBoundary } from './ErrorBoundary';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <App />
   </React.StrictMode>,
 );
